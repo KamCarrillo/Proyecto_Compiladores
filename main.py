0492f47d36e3ce257b227acc17d15b7d9a772024
ErickLeonG <erick.leon.gomez.03@gmail.com>
2024-03-04 18:52:18 -0600

    Initial version of the main program, the input and output file. The program splits by line, punctuation, spaces and operators

--- main.py
+++ main.py
@@ -21,11 +21,12 @@ if __name__ == '__main__':
 
     # Create a list of lists
     lines = [[]]
-    #counter to track the index of the list
-    i = 0
     for line in data:
         # Replace multiple spaces with a single space
         line = re.sub(' +', ' ', line)
+        # Remove the comments
+        if line.find('//') != -1:
+            line = line[:line.find('//')]
         # Split the line by space
         line = line.split(' ')
         # For each word in the line
@@ -35,10 +36,39 @@ if __name__ == '__main__':
             for char in word:
                 # If the character is ;
                 if char == ';':
-                    # Append the pending string to the list
-                    lines[i].append(pendingString)
+                    # Append the pending string to the list if it is not empty
+                    if pendingString != "":
+                        lines[len(lines)-1].append(pendingString)
                     # Append the ; to the list
-                    lines[i].append(';')
+                    lines[len(lines)-1].append(';')
                     # Break the reading of the line
-                    i += 1
+                    lines.append([])
+                    pendingString = ""
                     break
+                elif char in punctuation:
+                    # If the character is a punctuation append the pending string to the list if it is not empty
+                    if pendingString != "":
+                        lines[len(lines)-1].append(pendingString)
+                    # Append the punctuation to the list
+                    lines[len(lines)-1].append(char)
+                    # Reset the pending string
+                    pendingString = ""
+                elif char in operators:
+                    # If the character is an operator append the pending string to the list if it is not empty
+                    if pendingString != "":
+                        lines[len(lines)-1].append(pendingString)
+                    # Append the operator to the list
+                    lines[len(lines)-1].append(char)
+                    # Reset the pending string
+                    pendingString = ""
+                else:
+                    # If the character is a letter or number
+                    # Append the character to the pending string
+                    pendingString += char
+            if pendingString != "":
+                lines[len(lines)-1].append(pendingString)
+        lines.append([])
+    # Remove the empty lists
+    lines = [x for x in lines if x != []]
+    # Print the list of lists
+    print(lines)
